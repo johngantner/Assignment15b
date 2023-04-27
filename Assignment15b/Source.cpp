6e8e9ddbd2f4afefa1dd6826470747e1c53c8b8c
John gantner <jgantner73@gmail.com>
2023-04-27 18:44:29 -0400

    Creating all the templates

---
+++ Assignment15b/Source.cpp
@@ -0,0 +1,26 @@
+
+// John Gantner
+// 4/27/2023
+// C++ Software Development
+
+using namespace std;
+
+template <typename T>// Creating the primary template
+T half(T x) {
+	return x / 2;
+}
+
+template <>
+float half<float>(float x) {
+	return x / 2.0f;
+}
+
+template <>
+double half<double>(double x) {
+	return x / 2.0f;
+}
+
+template <>
+int half<int>(int x) {
+	return x / 2.0f;
+}
